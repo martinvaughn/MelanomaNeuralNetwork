378503dc700b96aeac92e5471a8b2c8c6efa5cfa
Martin Vaughn <59239114+martinvaughn@users.noreply.github.com>
2020-03-25 08:44:16 -0700

    Update make_prediction.py

--- make_prediction.py
+++ make_prediction.py
@@ -10,7 +10,7 @@ model = tf.keras.models.load_model('v2_cancer_model.h5')
 def prepare(filepath):
     img_array = cv2.imread(filepath)
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3) #perhaps?  return np.array(new_array).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3)
 
 
 prediction = model.predict([prepare('/Users/martinvaughn/Project_Pictures/Validate_Benign/IMG_5650.jpg')])
